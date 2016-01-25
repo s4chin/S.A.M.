4d9bf4565c9111c67b134c3d0f8c36adfa657de1
casey26 <kritikbchouhan@gmail.com>
2016-01-25 22:19:49 +0530

    Add browser_open.py

--- src/browser_open.py
+++ src/browser_open.py
@@ -1,6 +1,5 @@
 # https://docs.python.org/2/library/webbrowser.html
-# input as: python browser_open.py youtube
-from sys import argv
+
 import webbrowser
-sitename = argv[1]
-webbrowser.open("http://" + sitename + ".com")
+def open_site(sitename)
+	webbrowser.open("http://" + sitename + ".com")
