d86492bcc37a91107b317a0622ea465d95ad28b4
Sachin Kelkar <sachinkel19@gmail.com>
2016-02-10 14:42:57 +0530

    Add web search to browser_open

--- src/browser_open.py
+++ src/browser_open.py
@@ -4,3 +4,9 @@ import webbrowser
 
 def open_site(sitename):
     webbrowser.open("http://" + sitename + ".com")
+
+def web_search(sitename, query):
+    if sitename == "wikipedia":
+        webbrowser.open("https://en.wikipedia.org/wiki/Special:Search/{}".format(query))
+    else:
+        webbrowser.open("https://" + sitename + ".com/search?q={}".format(query))
